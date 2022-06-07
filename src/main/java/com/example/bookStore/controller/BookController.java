139ccdaa55c6f3f9c0f49162b16001f9cf89226e
Mohit Gaind <mohit@Mohits-MacBook-Pro.local>
2022-06-07 08:07:06 +0530

    [Mohit] | Add second book

--- src/main/java/com/example/bookStore/controller/BookController.java
+++ src/main/java/com/example/bookStore/controller/BookController.java
@@ -14,8 +14,10 @@ public class BookController {
     @GetMapping("api/v1/books")
     public ResponseEntity<List<Object>> getBooks(){
         BookDto book = BookDto.builder().title("First Book").build();
+        BookDto secondBook = BookDto.builder().title("Second Book").build();
 
         List<Object> books = new ArrayList<>();
+        books.add(secondBook);
         books.add(book);
 
         return ResponseEntity.ok(books);
