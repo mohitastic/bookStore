03b1612b054896cca6e85ffdfb38cf2d07e513f5
Mohit Gaind <mohit@Mohits-MacBook-Pro.local>
2022-06-07 07:20:56 +0530

    [Mohit] | IntitCal setup

---
+++ src/main/java/com/example/bookStore/controller/BookController.java
@@ -0,0 +1,23 @@
+package com.example.bookStore.controller;
+
+import com.example.bookStore.dto.BookDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class BookController {
+
+    @GetMapping("api/v1/books")
+    public ResponseEntity<List<Object>> getBooks(){
+        BookDto book = BookDto.builder().title("First Book").build();
+
+        List<Object> books = new ArrayList<>();
+        books.add(book);
+
+        return ResponseEntity.ok(books);
+    }
+}
